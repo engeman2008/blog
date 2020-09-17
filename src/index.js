8c4e9097adb202c57ec6799f758b95dd1ed48c3f
Eman <eman.cse2008@gmail.com>
2020-09-17 21:42:42 +0200

    request and response interceptoor

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+const requestInterceptor = axios.interceptors.request.use(request => {
+    // console.log(request);
+    //edit request config
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+axios.interceptors.request.eject(requestInterceptor);
+
+const responseInterceptor = axios.interceptors.response.use(response => {
+    console.log(response);
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+})
+axios.interceptors.response.eject(responseInterceptor);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
