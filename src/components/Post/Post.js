be20198339fc1dd6e77c43090bbf6e406b0c7e32
Eman <eman.cse2008@gmail.com>
2020-09-18 18:19:54 +0200

    navigate programtically

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,7 +4,7 @@ import './Post.css';
 
 const post = (props) => {
     // console.log(props)
-    return <article className="Post" >
+    return <article className="Post" onClick={props.clicked} >
         <h1>{props.title}</h1>
         <div className="Info">
             <div className="Author">{props.author}</div>
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
 
 import axios from '../../../axios'
 import Post from '../../../components/Post/Post';
@@ -25,16 +24,24 @@ class Posts extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        //navigate programtically
+        // this.props.history.push({pathname: '/' + id});
+        this.props.history.push('/' + id);
+
+    }
+
     render() {
         let posts = this.state.posts.map(
             post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
-                        <Post
-                            
-                            title={post.title}
-                            author={post.author}/>
-                    </Link>
+                    // <Link to={'/' + post.id} key={post.id}>
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)} />
+                    // </Link>
                 );
             });
         return (
