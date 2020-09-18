139ac12129626439e438c649bfb8812dfb493124
Eman <eman.cse2008@gmail.com>
2020-09-18 16:49:13 +0200

    routs and props

---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import axios from '../../../axios'
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        // console.log(this.props)
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return { ...post, author: 'Eman' }
+                });
+                this.setState({ posts: updatedPosts });
+            })
+            .catch(error => {
+                // this.setState({ error: true });
+                console.log(error)
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+    render() {
+        let posts = this.state.posts.map(
+            post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />;
+            });
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        );
+    }
+}
+
+export default Posts;
