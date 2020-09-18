7e810ddde6dbc903d5078c7e7b59a9226534105d
Eman <eman.cse2008@gmail.com>
2020-09-18 17:04:00 +0200

    styling active route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Link } from "react-router-dom";
+// import { Route, Link } from "react-router-dom"; nav link provide some styling, the same functionlity as link
+import { Route, NavLink } from "react-router-dom";
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -13,12 +14,17 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                        {/* activeClassName
+                        activeStyle */}
+                            <li><NavLink exact to="/" activeClassName='active'>Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
+                                // pathname: this.props.match.url +  '/new-post', //relative path
                                 hash : '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
+                             {/* relative path */}
+                            {/* <Link to={props.match.url + '/new'}></Link> */}
 
                             {/* <li><Link to='/new-post'>New Post</Link></li> */}
                         </ul>
