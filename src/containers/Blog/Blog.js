5d86b41190464526f1729a4456b3835b37cb752c
Eman <eman.cse2008@gmail.com>
2020-09-18 18:07:14 +0200

    routes switch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import { Route, Link } from "react-router-dom"; nav link provide some styling, the same functionlity as link
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -15,16 +15,16 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                        {/* activeClassName
+                            {/* activeClassName
                         activeStyle */}
                             <li><NavLink exact to="/" activeClassName='active'>Home</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 // pathname: this.props.match.url +  '/new-post', //relative path
-                                hash : '#submit',
+                                hash: '#submit',
                                 search: '?quick-submit=true'
                             }}>New Post</NavLink></li>
-                             {/* relative path */}
+                            {/* relative path */}
                             {/* <Link to={props.match.url + '/new'}></Link> */}
 
                             {/* <li><Link to='/new-post'>New Post</Link></li> */}
@@ -33,9 +33,12 @@ class Blog extends Component {
                 </header>
                 {/* <Route path='/' exact render={() => <h1>Home</h1>} />
                 <Route path='/' render={() => <h1>Home2</h1>} /> */}
-                <Route path='/' exact component={Posts} />
-                <Route path='/new-post' exact component={NewPost} />
-                <Route path='/:id' exact component={FullPost} />
+                {/* the order is important inside switch */}
+                <Switch>
+                    <Route path='/' exact component={Posts} />
+                    <Route path='/new-post' exact component={NewPost} />
+                    <Route path='/:id' exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
