2d29bcf8b57f284005c6c21ab79b522b879715dd
Eman <eman.cse2008@gmail.com>
2020-09-19 10:41:43 +0200

    handle 404 case

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,9 @@ import Posts from './Posts/Posts';
 import NewPost from '../Blog/NewPost/NewPost';
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
     render() {
 
         return (
@@ -34,9 +37,10 @@ class Blog extends Component {
                 <Route path='/' render={() => <h1>Home2</h1>} /> */}
                 {/* the order is important inside switch */}
                 <Switch>
-                    <Route path='/new-post' component={NewPost} />
+                    {this.state.auth ? <Route path='/new-post' component={NewPost} /> : null}
                     <Route path='/posts' component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h1>Not Found</h1>} />
+                    {/* <Redirect from="/" to="/posts" /> */}
                     {/* <Route path='/' component={Posts} /> */}
                 </Switch>
             </div>
